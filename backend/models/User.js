2e41b12e5fa4802fb832479cff2a87f3a926e3c2
vinayak-dhar <vinayakdhar003@gmail.com>
2026-03-08 17:15:06 +0530

    added user and task schema model

---
+++ backend/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, default: null },
+        profileImageUrl: { type: String, default: null },
+        role: { type: String, enum: ["admin", "member"], default: "member" }, // Role-based access
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model("User", UserSchema);
