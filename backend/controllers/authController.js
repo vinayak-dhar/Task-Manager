a36d2df1c4f4df698de70a47e824dd2e2a365799
vinayak-dhar <vinayakdhar003@gmail.com>
2026-03-08 18:20:40 +0530

    implemented generateToken and registerUser function in auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -11,7 +11,46 @@ const generateToken = (userId) => {
 // @route POST /api/auth/register
 // @access Public
 const registerUser = async (req, res) => {
-    
+    try {
+        const { name, email, password, profileImageUrl, adminInviteToken } = req.body;
+
+        // check if user already exists
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        // Determine user role: admin if correct token is provide, otherwise member
+        let role = "member";
+        if (adminInvitetoken && adminInviteToken == process.env.ADMIN_INVITE_TOKEN) {
+            role = "admin";
+        }
+
+        // Hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // create new user
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+            profileImageUrl,
+            role
+        });
+
+        // Return user data with JWT
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            profileImageUrl: user.profileImageUrl,
+            token: generateToken(user._id)
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
 };
 
 // @desc Login User
