a4c0fccc18a5cb4ea30aa55872af09136881500c
vinayak-dhar <vinayakdhar003@gmail.com>
2026-03-08 16:28:10 +0530

    backend and frontend setup

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+// Middleware to handle CORS
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+// Middleware 
+app.use(express.json());
+
+// Routes
+// app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/tasks", taskRoutes);
+// app.user("/api/reports", reportRoutes);
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
