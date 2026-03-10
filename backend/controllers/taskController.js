8ecfbbaebe7e635465a4f19a9b63e9a1981eabb3
vinayak-dhar <vinayakdhar003@gmail.com>
2026-03-10 21:00:24 +0530

    implemented get task by id route in taskRoutes and implemented getTaskById in taskController

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -81,7 +81,16 @@ const getTasks = async (req, res) => {
 // @access private
 const getTaskById = async (req, res) => {
     try {
+        const task = await Task.findById(req.params.id).populate(
+            "assignedTo",
+            "name email profileImageUrl"
+        );
 
+        if (!task) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+
+        res.json(task);
     } catch (error) {
         res.status(500).json({ message: "Server error", error: error.message });
     }
