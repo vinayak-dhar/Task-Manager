0d739d3a8a8d4ea66be6f16be6daf620106cf493
vinayak-dhar <vinayakdhar003@gmail.com>
2026-03-19 22:16:13 +0530

    feat(dashboard): implement layout with role-based sidebar and navigation
    - added DashboardLayout with Navbar and SideMenu
    - implemented role-based menu rendering for admin and users
    - integerated logout functionality and responsive mobile sidebar
    - fetched dashboard data using API

--- frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useUserAuth } from '../../hooks/useUserAuth'
+import { UserContext } from '../../context/userContext';
+import DashboardLayout from '../../components/layouts/DashboardLayout';
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../../Utils/axiosinstance';
+import { API_PATHS } from '../../Utils/apiPaths';
+import moment from 'moment';
 
 const Dashboard = () => {
   useUserAuth(); 
   
-  return (
-    <div>Dashboard</div>
-  )
+  const { user } = useContext(UserContext);
+
+  const navigate = useNavigate();
+
+  const [dashboardData, setDashboardData] = useState(null);
+  const [pieChartData, setPieChartData] = useState([]);
+  const [barChartData, setBarChartData] = useState([]);
+
+  const getDashboardData = async () => {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.TASKS.GET_DASHBOARD_DATA
+      );
+
+      if (response.data) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+
+  useEffect(() => {
+    getDashboardData();
+
+    return () => {}
+  }, []);
+
+  return <DashboardLayout activeMenu={"Dashboard"}>
+    
+  </DashboardLayout>
 }
 
 export default Dashboard
