f45b483cc296ab3f6636c6f23972b465e746054f
vinayak-dhar <vinayakdhar003@gmail.com>
2026-03-30 22:16:35 +0530

    fix: resolve todoCheckList field mismatch to ensure checklist displays correctly in task cards

--- frontend/Task-Manager/src/pages/Admin/ManageTasks.jsx
+++ frontend/Task-Manager/src/pages/Admin/ManageTasks.jsx
@@ -103,7 +103,7 @@ const ManageTasks = () => {
               assignedTo={item.assignedTo?.map((item) => item.profileImageUrl)}
               attachmentCount={item.attachments?.length || 0}
               completedTodoCount={item.completedTodoCount || 0}
-              todoChecklist={item.todoChecklist || []}
+              todoChecklist={item.todoCheckList || []}
               onClick={() => {
                 handleClick(item)
               }}
